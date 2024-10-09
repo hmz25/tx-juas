77bdf5f32f0244bc83939b17541584892b171d66
hmz25 <hmz25@cornell.edu>
2024-10-09 10:43:53 -0400

    updating scripts for: - handheld_spectral index = creating a pixel-based classifier for index calculation - orbital index overlay = applying pixel-based classifier to both ortho image and las image, then taking sum of index values to compare

---
+++ scripts/hz_handheld_spectralindex.R
@@ -0,0 +1,275 @@
+
+# load libraries ----------------------------------------------------------
+
+library(raster)
+library(readr)
+#library(tidyverse)
+library(terra)
+library(dplyr)
+library(tidyr)
+library(purrr)
+library(stringr)
+#library(matrixStats)
+#install.packages("randomForest")
+library(randomForest)
+library(lubridate)
+library(ggplot2)
+
+
+# set working directory ---------------------------------------------------
+
+#set wd
+setwd("C:/Users/hmz25")
+
+
+
+# manual cone density data ------------------------------------------------
+
+#add in cone density per quadrat data
+
+quadrat_cones <- read_csv("Box/texas/pollen_production/TX jan 24/data analysis/2024_quadratcones.csv")
+
+#add cones per weight columns
+quadrat_cones <- quadrat_cones %>%
+  select(date_collected,
+         site,
+         tree,
+         quadrat,
+         total_mass,
+         s1_count, s1_weight,
+         s2_count, s2_weight,
+         s3_count, s3_weight,
+         s4_count, s4_weight,
+         s5_count, s5_weight) %>%
+  mutate(s1 = (quadrat_cones$s1_count/quadrat_cones$s1_weight),
+         s2 = (quadrat_cones$s2_count/quadrat_cones$s2_weight),
+         s3 = (quadrat_cones$s3_count/quadrat_cones$s3_weight),
+         s4 = (quadrat_cones$s4_count/quadrat_cones$s4_weight),
+         s5 = (quadrat_cones$s5_count/quadrat_cones$s5_weight))  
+
+#take mean of cone per weight for each quadrat
+quadrat_cones <- quadrat_cones %>%
+  select(date_collected,
+         site,
+         tree,
+         quadrat,
+         total_mass,
+         s1,
+         s2,
+         s3,
+         s4,
+         s5)
+
+quadrat_cones$cone_mean <- rowMeans(subset(quadrat_cones, select = c("s1", "s2", "s3", "s4", "s5")),
+                                    na.rm = TRUE)
+
+#take standard deviation of quadrat cones
+quadrat_cones <- quadrat_cones %>%
+  rowwise %>%
+  mutate(cone_sd = sd(c_across(s1:s5))) %>%
+  ungroup()
+
+#calculate total number of cones in each quadrat
+quadrat_cones <- quadrat_cones %>%
+  mutate(total_cones = cone_mean*total_mass)
+
+#rename date column and ensure dates are in the right format
+quadrat_cones <- rename(quadrat_cones, date = date_collected)
+quadrat_cones$date <- mdy(quadrat_cones$date)
+
+#rename site column to match the site column for the images
+quadrat_cones$site <- substr(quadrat_cones$site, 1, 4)
+
+
+
+# process drone image files -----------------------------------------------
+
+#create a function to load image files
+list.files("Box/texas/pollen_production/TX jan 24/data analysis/cropped eye levels") #83 files
+
+eyelevel_files <- list.files("Box/texas/pollen_production/TX jan 24/data analysis/cropped eye levels")#[1]
+image_directory <- "Box/texas/pollen_production/TX jan 24/data analysis/cropped eye levels/"
+
+
+test <- stack("Box/texas/pollen_production/TX jan 24/data analysis/cropped eye levels/kimble_t3_q3.tif")
+plotRGB(test)
+
+
+test2 <- test
+test2$kimble_t3_q3_3[test2$kimble_t3_q3_1 > 100 & test2$kimble_t3_q3_2 > 100 & test2$kimble_t3_q3_3 > 100] <- 255
+test2$kimble_t3_q3_1[test2$kimble_t3_q3_1/test2$kimble_t3_q3_2 > 1.5] <- 255
+plotRGB(test2)
+
+test3 <- test$kimble_t3_q3_1/test$kimble_t3_q3_3
+plot(test3)
+
+
+
+# rf pixel mask -----------------------------------------------------------
+
+
+### create a model to mask non foliage and non cones
+
+#training dataset for non-foliage and non-cones
+sup_not_twig <- stack("Box/texas/pollen_production/TX jan 24/data analysis/not_2.tiff")
+#plotRGB(sup_not_twig) #sup_not_twig$not_2_1 #sup_not_twig$not_2_1[1:100]
+
+sup_not_twig_df <- as.data.frame(sup_not_twig) %>%
+  filter(not_2_1 != 255) %>%  rename(c("r" =1 , "g" = 2, "b" = 3, "max" = 4)) %>%
+  mutate(class = "not") 
+#head(sup_not_twig_df)
+
+#training dataset for foliage and cones
+sup_yes_twig <- stack("Box/texas/pollen_production/TX jan 24/data analysis/yes_1.tif")
+#plotRGB(sup_yes_twig) #sup_yes_twig$yes_1_1 #sup_yes_twig$yes_1_1[1:100]
+
+sup_yes_twig_df <- as.data.frame(sup_yes_twig) %>%
+  filter(yes_1_1 != 255) %>%
+  rename(c("r" =1 , "g" = 2, "b" = 3, "max" = 4)) %>%
+  mutate(class = "yes")  #head(sup_yes_twig_df)
+
+#combine training datasets and randomly select pixels from theme
+training_df <- bind_rows(sup_not_twig_df, sup_yes_twig_df) %>%
+  dplyr::select(-max) %>%
+  sample_n(100000) %>%
+  mutate(class = as.factor(class))
+# head(training_df)
+# str(training_df)
+
+#run a pixel-based classifier
+rf_mask <- randomForest(class ~ ., data = training_df, na.action=na.omit)
+rf_mask
+
+
+### apply model to a single photo
+photo_i <- stack("Box/texas/pollen_production/TX jan 24/data analysis/cropped eye levels/sonora_t6_q8.tif")
+#plotRGB(photo_i)
+
+#make sure layer names for picture match the training df column names 
+names(photo_i) <- colnames(training_df)[1:3]
+photo_i_mask <- predict(photo_i, rf_mask)
+plot(photo_i_mask)
+
+#add mask layer back to original photo
+photo_i <- stack(photo_i, photo_i_mask)
+plot(photo_i)
+
+
+
+# applying rf pixel mask to all handheld drone images ---------------------
+
+
+### apply model to all cropped handheld photos and save in a new folder
+# photo_dir <- "Box/texas/pollen_production/TX jan 24/data analysis/cropped eye levels"
+# photo_list <- list.files(photo_dir, full.names = FALSE)
+# photo_list_full_dir <- list.files(photo_dir, full.names = TRUE)
+# 
+# 
+# for(i in 1:length(photo_list)){
+#   photo_i <- stack(photo_list_full_dir[i])  #plotRGB(test)
+#   
+#   photo_i_df <- as.data.frame(photo_i) %>%
+#     rename(c("r" = 1 , "g" = 2, "b" = 3))  #head(test_df)
+#   
+#   photo_i_mask_df <- predict(rf_mask, photo_i_df)
+#   # head(photo_i_mask_df)
+#   # photo_i_df <- photo_i_df %>%
+#   #   mutate(is_foreground = photo_i_mask_df,
+#   #          is_foreground_numeric = case_when( is_foreground == "yes" ~ 1,
+#   #                                             is_foreground == "not" ~ 0))
+#   # #head(photo_i_df)
+#   
+#   photo_i <- addLayer(photo_i, photo_i[[3]])
+#   photo_i[[4]] <- photo_i_mask_df # photo_i_df$is_forground
+#   
+#   save_file_name = paste0("Box/texas/pollen_production/TX jan 24/data analysis/cropped masked eye level/",photo_list[i])
+#   raster::writeRaster(photo_i, save_file_name, overwrite = TRUE)
+#   print(i)
+# }
+
+
+
+### calculate rgb values and an index for each photo ####
+photo_dir <- "Box/texas/pollen_production/TX jan 24/data analysis/cropped masked eye level/"
+photo_list <- list.files(photo_dir, full.names = FALSE)
+photo_list_full_dir <- list.files(photo_dir, full.names = TRUE)
+
+photo_masked_df <- data.frame()
+
+for(i in 1:length(photo_list)){
+  photo_i <- stack(photo_list_full_dir[i]) #plot(photo_i) #photo_i <- stack(photo_list_full_dir[2])
+  
+  photo_i[[1]][photo_i[[4]] == 1] <- NA
+  photo_i[[2]][photo_i[[4]] == 1] <- NA
+  photo_i_index_rg_dif <- (photo_i[[1]] - photo_i[[2]])/(photo_i[[1]] + photo_i[[2]])
+  
+  photo_i_rg_dif <- cellStats(photo_i_index_rg_dif, "mean")
+  
+  photo_i_index_rg_thresh <- photo_i_index_rg_dif #plot(photo_i)
+  photo_i_index_rg_thresh[photo_i_index_rg_dif[] < 0.05] <- 0
+  photo_i_index_rg_thresh[photo_i_index_rg_dif[] > 0.05] <- 1
+  photo_i_index_rg_thresh_mean <- cellStats(photo_i_index_rg_thresh, "mean") #calculate  #plot(photo_i_index_rg_thresh)
+  photo_i_index_rg_thresh_sum <- photo_i_index_rg_thresh_mean* 
+    ((ncell(photo_i_index_rg_thresh) - summary(photo_i_index_rg_thresh)[6]) / ncell(photo_i_index_rg_thresh)) #multiply by portion of cells not NA
+  #(ncell(photo_i_index_rg_thresh) - ncell(photo_i_index_rg_thresh, na.omit = TRUE)) #plotRGB(photo_i) #ncell(photo_i) #cellStats(photo_i_index_rg_thresh, "mean")
+  
+  
+  photo_i_r <- cellStats(photo_i[[1]], "mean")
+  photo_i_g <- cellStats(photo_i[[2]], "mean")
+  photo_i_b <- cellStats(photo_i[[3]], "mean")
+  photo_i_mask <- cellStats(photo_i[[4]], "mean") - 1 #1 == background, 2 = foreground
+  
+  photo_i_rgb <- data.frame(photo_list[i], photo_i_r, photo_i_g, photo_i_b, photo_i_mask, photo_i_rg_dif, 
+                            photo_i_index_rg_thresh_mean, photo_i_index_rg_thresh_sum)
+  photo_masked_df <- bind_rows(photo_masked_df, photo_i_rgb)
+  print(i)
+}
+
+
+
+#add values to dataframe that has the number of cones per quadrat
+photo_masked_df_join <- photo_masked_df %>%
+  mutate(site = substr(photo_list.i., 1, 4),
+         tree = as.numeric(stringr::str_sub(photo_list.i., -8, -8)),
+         quadrat = stringr::str_sub(photo_list.i., -5, -5))
+
+quadrat_cones_rgb <- left_join(quadrat_cones, photo_masked_df_join) %>%
+  filter(!is.na(total_cones)) %>%
+  filter(!is.na(photo_i_r)) %>% mutate(photo_i_mask = photo_i_mask - 1)
+
+quadrat_cones_rgb_test <- quadrat_cones_rgb %>%  
+  filter((photo_i_r + photo_i_g + photo_i_b) > 0)
+
+#quadrat_cones_rgb_test <- quadrat_cones_rgb_test %>% filter(photo_i_mask > 0.5) #applying this filter results in no values 
+
+ggplot(quadrat_cones_rgb_test, aes(x=photo_i_index_rg_thresh_sum, y = total_cones/(0.25*0.25))) + 
+  geom_point(alpha = 0.5) + 
+  theme_bw() + 
+  geom_smooth(method = "lm", se = FALSE) +
+  xlab("spectral index") + ylab(cone~density~(cones/m^2)) + ggthemes::theme_few()#scale_color_viridis_c()  # + facet_wrap(~site)
+
+
+fit <- lm(quadrat_cones_rgb_test$total_cones  ~ quadrat_cones_rgb_test$photo_i_index_rg_thresh_sum   )
+summary(fit)
+
+#fit$residuals
+quadrat_cones_rgb_test  <- quadrat_cones_rgb_test %>%
+  mutate(photo_i_rg_dif_resid = fit$residuals,
+         photo_i_rg_dif_pred = fit$fitted.values)
+
+
+
+cor(quadrat_cones_rgb_test)
+quadrat_cones_rgb_test %>% dplyr::select(10:23, - photo_list.i.) %>% 
+  PerformanceAnalytics::chart.Correlation(.)
+
+
+test <- raster::stack("C:/Users/dsk273/Box/texas/pollen_production/TX jan 24/data analysis/cropped masked eye level/kimble_t6_q7.tif")
+plot(test)
+
+test2 <- test
+test2[[1]][test2[[4]] == 1] <- NA
+test2[[2]][test2[[4]] == 1] <- NA
+test2_index_rg_dif <- (test2[[1]] - test2[[2]])/(test2[[1]] + test2[[2]])
+
+plot(test2_index_rg_dif)
--- scripts/orbital_index_overlay.R
+++ scripts/orbital_index_overlay.R
@@ -1,13 +1,19 @@
 #code to apply spectral index to orbital reconstructions of trees
 
-#load libraries
+# load libraries ----------------------------------------------------------
 library(lidR)
 library(terra)
 library(sf)
 library(raster)
 library(dplyr)
+library(randomForest)
 
 #browseVignettes(package = "lidR")
+
+
+
+# LAS data ----------------------------------------------------------------
+ 
 
 #read in LAS data from orbital reconstruction
 wade_t3_las <- readLAS("C:/Users/hmz25/Documents/pix4d/20240103_Wade_tree3_orbital/2_densification/point_cloud/20240103_Wade_tree3_orbital_group1_densified_point_cloud.las",
@@ -20,17 +26,16 @@ plot(wade_t3_las)
 #plot(wade_t3, color = RGB)
 
 #clip LAS to extent of focal tree
-##load in shape file
+##load in shape file of manually segmented focal tree
 wade_t3_shp <- st_read("C:/Users/hmz25/Desktop/TX 2024 analysis/qgis/wade_t3_extent.shp")
 
 ##reproject to same crs
 #projection(wade_t3) <- crs(wade_t3_ext)
 #wade_t3_las_reproj <- st_transform(wade_t3_las, projection(wade_t3_ext))
-
 wade_t3_shp_reproj <- st_transform(wade_t3_shp, st_crs(wade_t3_las))
 compareCRS(wade_t3_las, wade_t3_shp_reproj) #false but it's okay
 
-#clip las point cloud to be just focal tree
+#clip las point cloud to focal tree
 wade_t3_las_clip <- clip_roi(wade_t3_las, wade_t3_shp_reproj)
 plot(wade_t3_las_clip) #yay!
 ##can we apply index to the las plot? 
@@ -42,55 +47,144 @@ R <- wade_t3_las@data$R
 G <- wade_t3_las@data$G
 B <- wade_t3_las@data$B
 
-#create orange index
-orange_index <- function(R, G) {
-  (R - G)/(R + G)
-}
 
-#apply index to orbital
-index_overlay <- orange_index(R, G)
-plot(index_overlay)
-print(index_overlay)
+# pixel classifier --------------------------------------------------------
 
-##use equation to get number of cones per pixel 
-##figure out pixel size in las file
-##
+#run pixel classifier to filter out non cone and foliage pixels from images
 
-#take mean index value of orbital (to compare to mean index value of same tree from ortho) 
-mean_index_orbital <- mean(index_overlay)
-sum_index_orbital <- sum(index_overlay)
+#load pictures to create training dataset for non-foliage and non-cones
+sup_not_twig <- stack("C:/Users/hmz25/Box/texas/pollen_production/TX jan 24/data analysis/not_2.tiff")
+#plotRGB(sup_not_twig) 
+#sup_not_twig$not_1_1 
+#sup_not_twig$not_1_1[1:100]
+
+#dataframe for non-foliage and non-cone pixels
+sup_not_twig_df <- as.data.frame(sup_not_twig) %>%
+  rename(c("r" =1 , "g" = 2, "b" = 3, "max" = 4)) %>%
+  filter(r != 255) %>% 
+  mutate(class = "not") 
+#head(sup_not_twig_df)
+
+#load pictures to create training dataset for foliage and cones
+sup_yes_twig <- stack("C:/Users/hmz25/Box/texas/pollen_production/TX jan 24/data analysis/yes_1.tif")
+#plotRGB(sup_yes_twig) 
+#sup_yes_twig$yes_1_1 
+#sup_yes_twig$yes_1_1[1:100]
+
+#dataframe for foliage and cone pixels
+sup_yes_twig_df <- as.data.frame(sup_yes_twig) %>%
+  rename(c("r" =1 , "g" = 2, "b" = 3, "max" = 4)) %>%
+  filter(r != 255) %>% 
+  mutate(class = "yes")  
+#head(sup_yes_twig_df)
+
+#combine training datasets and randomly select pixels from theme
+training_df <- bind_rows(sup_not_twig_df, sup_yes_twig_df) %>%
+  dplyr::select(-max) %>%
+  sample_n(100000) %>%
+  mutate(class = as.factor(class))
+#RENAME SO THAT LAYER YES = 1, NO = 0 INSTEAD OF DEFAULT 1 AND 2 
+#head(training_df)
+#str(training_df)
+
+#run a pixel-based classifier
+rf_mask <- randomForest(class ~ ., data = training_df, na.action=na.omit)
+#rf_mask
+
+
+# apply pixel classifier to LAS orbital image --------------------------
+
+#create df of las pixel values
+wade_t3_las_clip_df <- as.data.frame(wade_t3_las_clip@data) %>%
+  rename(c("r" = 4 , "g" = 5, "b" = 6)) %>% 
+  select("r", "g", "b") %>% 
+  mutate(r8 = r/256,
+         g8 = g/256,
+         b8 = b/256) %>% #convert 16 bit color in las to 8 bit color in raster  
+  select("r8", "g8", "b8") %>% 
+  rename(c("r" = 1, "g" = 2, "b" = 3))
+#head(wade_t3_las_clip_df)
+
+#create df with pixel values assigned to cone/foliage vs not cone/foliage 
+wade_t3_las_clip_mask_df <- predict(rf_mask, wade_t3_las_clip_df) %>% 
+  as.data.frame()
+# head(photo_i_mask_df)
+# photo_i_df <- photo_i_df %>%
+#   mutate(is_foreground = photo_i_mask_df,
+#          is_foreground_numeric = case_when( is_foreground == "yes" ~ 1,
+#                                             is_foreground == "not" ~ 0))
+# #head(photo_i_df)
+
+wade_t3_las_mask_df <- wade_t3_las_clip_mask_df %>% 
+  cbind(wade_t3_las_clip_df, wade_t3_las_clip_mask_df) %>% 
+  select(c(1:4)) %>% 
+  rename("rf_mask" = 1)
+
+#add pixel classification values back to las data 
+wade_t3_las_clip@data$rf_mask <- wade_t3_las_clip_mask_df # photo_i_df$is_foreground
+#str(wade_t3_las_clip)
+#print(wade_t3_las_clip@data$rf_mask)
+
+# ortho data --------------------------------------------------------------
 
 #load in ortho from wade
-wade_ortho <- brick("C:/Users/hmz25/Documents/pix4d/20240103_Wade/3_dsm_ortho/2_mosaic/20240103_Wade_transparent_mosaic_group1.tif")
+wade_ortho <- stack("C:/Users/hmz25/Documents/pix4d/20240103_Wade/3_dsm_ortho/2_mosaic/20240103_Wade_transparent_mosaic_group1.tif")
 #plotRGB(wade_ortho)
 
 #crop ortho down to smaller size 
 wade_ortho_clip <- crop(wade_ortho, extent(wade_t3_shp_reproj))
 #plotRGB(wade_ortho_clip)
-#plot(wade_t3_ext
 
 #extract t3 from ortho
 wade_t3_rgb <- mask(wade_ortho_clip, wade_t3_shp_reproj)
 plotRGB(wade_t3_rgb)
 
-#extract pixel values from ortho image
-t3_ortho_rgb <- raster::extract(x = wade_ortho_clip, 
-                                y = wade_t3_shp_reproj, 
-                                df = TRUE)
-#head(t3_ortho_rgb)
+#apply pixel classifier to ortho image
+wade_t3_rgb_sub <- subset(wade_t3_rgb, 1:3) 
+names(wade_t3_rgb_sub) <- colnames(training_df)[1:3]
+wade_t3_rgb_masked <- predict(wade_t3_rgb_sub, rf_mask)
+#plot(wade_t3_rgb_masked)
 
-#clean data frame and add column for spectral index 
-t3_rgb_df <- t3_ortho_rgb %>% 
-  rename("r" = 2, "g" = 3, "b" = 4) %>% 
-  select("r", "g", "b") %>% 
-  mutate(index = (r-g)/(r+g))
+wade_t3_rgb <- stack(wade_t3_rgb_sub, wade_t3_rgb_masked)
 
-#take mean index value for t3 from ortho image
-mean_index_ortho <- mean(t3_rgb_df$index) #wow they're really close
+#create dataframe for rgb image
+wade_t3_rgb_df <- as.data.frame(wade_t3_rgb) %>% 
+  na.omit() %>% 
+  rename("rf_mask" = "layer_value")
+
+
+
+# orange index -----------------------------------------------------
+
+
+#calculate index values for las orbital
+
+## add index column for las masked df 
+wade_t3_las_mask_df <- wade_t3_las_mask_df %>% 
+  filter(rf_mask == "yes") %>% 
+  mutate(orange_index = (r-g)/(r+g))
+
+#take sum index value of orbital (to compare to sum index value of same tree from ortho) 
+sum_index_las <- sum(wade_t3_las_mask_df$orange_index)
+
+
+#calculate index values for rgb ortho image 
+
+wade_t3_rgb_mask_df <- wade_t3_rgb_df %>% 
+  filter(rf_mask == "yes") %>% 
+  mutate(orange_index = (r-g)/(r+g))
+#head(wade_t3_rgb_mask_df)  
+
+#take sum index value for t3 from ortho image
+sum_index_rgb <- sum(wade_t3_rgb_mask_df$orange_index) 
+
+
+# visualize index on las and rgb images -----------------------------------
+
 
 #visualize index on wade t3
 
-#define index for ortho 
+#define index 
 ortho_index <- orange_index <- function(r, g) {
   index <- ((r-g)/(r+g))
   return(index)
@@ -115,6 +209,7 @@ cone_palette <- colorRampPalette(c("green", "orangered"))
 plot(wade_t3_ortho_index, col = cone_palette(100), alpha = 1, axes = FALSE, box = FALSE)
 
 #NEXT STEPS
-## same pixel classification for orbital as for ortho? YES
+## use equation to get number of cones per pixel 
+## figure out pixel size in las file
 ## create loops to do this for all orbital images rather than just one 
 
