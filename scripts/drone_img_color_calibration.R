b5090464232ef186870c7ff8c6f4530bd74a02e1
hmz25 <hmz25@cornell.edu>
2025-06-16 10:06:12 -0400

    updated script to analyze differences in index values between white quadrat in different flights

--- scripts/drone_img_color_calibration.R
+++ scripts/drone_img_color_calibration.R
@@ -53,8 +53,9 @@ img_df_full
 ggplot(img_df_full) +
   geom_histogram(aes(x = r)) +
   facet_wrap(~source) + 
-  xlim(230,255)
-
+  xlim(230,255) + 
+  ylim(0,40) +
+  theme_classic()
 
 img_df_full %>% 
   pivot_longer(cols = c("r","g", "b"), names_to = "channel", values_to = "digital_number") %>% 
@@ -63,24 +64,149 @@ img_df_full %>%
   facet_wrap(~source) + 
   theme_minimal()
 
+img_df_full %>% 
+  ggplot() +
+  geom_histogram(aes(x = (r-g)/(r+g))) + 
+  facet_wrap(~source) + 
+  theme_classic()
+
 #visualize as each site histograms overlaying each other, facet wrapped by channel?
 df_long <- pivot_longer(img_df_full, cols = c(r, g, b),
                         names_to = "channel", values_to = "digital_number")
 
-ggplot(df_long, aes(x = digital_number, fill = source)) +
-  geom_histogram(position = "identity", alpha = 0.6, bins = 30) +
-  facet_wrap(~channel, scales = "free_x") +
-  labs(x = "digital number value", y = "count", title = "rgb histograms for different images") +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold")) +
-  scale_fill_brewer(palette = "Set1")
 
-ggplot(df_long, aes(x = digital_number, fill = channel)) + 
-  geom_histogram(position = "identity", , alpha = 0.5) +
-  facet_wrap(~source, scales = "free_x") +
-  labs(x = "digital number value", y = "count", title = "rgb histograms for different images") +
-  theme_minimal() +
-  theme(strip.text = element_text(face = "bold"))
+#shadow pixel values vs non shadow pixel values from quadrats on the same day
+
+shadow <- rast("C:/Users/hmz25/Desktop/backdrops/shadow quadrats/cath_20241230_t2_white.tif")
+no_shadow <- rast("C:/Users/hmz25/Desktop/backdrops/cath_20241230_t4_white.tif")
+
+plotRGB(shadow)
+plotRGB(no_shadow)
+
+names(shadow) <- c("r", "g", "b")
+names(no_shadow) <- c("r", "g", "b")
+
+shadow_df <- as.data.frame(shadow) %>% 
+  mutate(source = "shadow") # %>% 
+  # pivot_longer(cols = r:b, values_to = "digital_number", names_to = "channel")
+
+no_shadow_df <- as.data.frame(no_shadow) %>% 
+  mutate(source = "no_shadow") # %>% 
+  # pivot_longer(cols = r:b, values_to = "digital_number", names_to = "channel")
+
+df <- bind_rows(shadow_df, no_shadow_df)
+
+ggplot(df) +
+  geom_histogram(aes(x = digital_number, fill = channel), alpha = 0.5) + 
+  facet_wrap(~source) +
+  scale_fill_manual(values = c(r = "red",
+                    g = "darkgreen",
+                    b = "blue")) +
+  theme_classic()
+
+ggplot(df_index) +
+  geom_histogram(aes(x = (r-g)/(r+g))) +
+  facet_wrap(~source) + 
+  theme_classic()
+
+# group by dates/sites, look at histograms between dates/sites
+
+dir <- "C:/Users/hmz25/Desktop/backdrops"
+files_list <- list.files(dir, pattern = "_white.tif", full.names = TRUE)
+
+df_full <- data.frame()
+
+for (i in seq_along(files_list)) {
+  
+  #load in image
+  img <- rast(files_list[i])
+  
+  #rename channels to RGB
+  names(img) <- c("r", "g", "b")
+  
+  #convert to data frame
+  img_df <- as.data.frame(img)
+  
+  #add image filename as a column
+  img_df$source <- basename(files_list[i])
+  
+  #add to cumulative data frame
+  df_full <- bind_rows(df_full, img_df)
+}
+
+df_full
+
+df_full_restr <- df_full %>% 
+  separate(source, into = c("site", "date", "tree", "drop"), sep = "_") %>%
+  select(-drop) %>% 
+  group_by(site, date) 
+
+df_full_restr %>% 
+  pivot_longer(cols = r:b, names_to = "channel", values_to = "digital_number") %>%  
+  ggplot() +
+  geom_histogram(aes(x = digital_number, fill = date), alpha = 0.5) + 
+  facet_wrap(~channel)
+
+df_full_restr %>% 
+  pivot_longer(cols = r:b, names_to = "channel", values_to = "digital_number") %>%  
+  ggplot() +
+  geom_jitter(aes(x = date, y = digital_number, color = channel)) + 
+  facet_wrap(~channel, ncol = 1) + 
+  scale_color_manual(values = c(r = "red", g = "darkgreen", b = "blue")) +
+  theme_classic()
+
+#visualize how index value changes on white based on different flights
+df_full_restr %>% 
+  ggplot() +
+  geom_jitter(aes(x = date, y = (r-g)/(r+g), color = date)) + 
+  theme_classic()
+
+df_full_restr %>% 
+  ggplot() +
+  geom_histogram(aes(x = (r-g)/(r+g))) +
+  facet_wrap(~date, ncol = 1) +
+  theme_classic()
+
+
+#looking at how white values change between adjusted and non-adjusted images
+adj_dir <- "C:/Users/hmz25/Desktop/quadrat_lighting/quadrat_white_ADJUSTED"
+adj_files_full <- list.files(adj_dir, pattern = "_white.tif", full.names = TRUE)
+
+adj_df_full <- data.frame()
+
+for (i in seq_along(adj_files_full)) {
+  
+  #load in image
+  adj_img <- rast(adj_files_full[i])
+  
+  #rename channels to RGB
+  names(adj_img) <- c("r", "g", "b")
+  
+  #convert to data frame
+  adj_img_df <- as.data.frame(adj_img)
+  
+  #add image filename as a column
+  adj_img_df$source <- basename(adj_files_full[i])
+  
+  #add to cumulative data frame
+  adj_df_full <- bind_rows(adj_df_full, adj_img_df)
+}
+
+adj_df_full
+
+ggplot(adj_df_full) +
+  geom_histogram(aes(x = r)) +
+  facet_wrap(~source) + 
+  xlim(230,255) + 
+  ylim(0,40) +
+  theme_minimal()
+
+adj_df_full %>% 
+  ggplot() +
+  geom_histogram(aes(x = (r-g)/(r+g))) + 
+  facet_wrap(~source) + 
+  theme_classic()
+
 
 # examine how canopy histograms change based on color calibration --------
 
