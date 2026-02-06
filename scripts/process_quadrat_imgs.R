8e6481cdef9c1864352dd3d096a24a797a0fc900
hmz25 <hmz25@cornell.edu>
2026-02-06 11:13:16 -0500

    updating quadrat image processing script to appropriately apply rf pixel classifier to adjusted and non-adjusted images

--- scripts/process_quadrat_imgs.R
+++ scripts/process_quadrat_imgs.R
@@ -46,6 +46,29 @@ plot(photo_i)
 # plot(photo_i$class) 
 # plot(photo_i$r)
 
+rf_mask_adj <- get(load("Katz lab/texas/rf_mask_adj.RData"))
+
+#apply model to a single photo
+photo_i_adj <- rast("Katz lab/texas/tx 2026 drone pics/2026 quadrat pics/cropped_quadrat_pics/cathedral_t8_tree_adj.tif")
+#plotRGB(photo_i_adj)
+
+#make sure layer names for picture match the training df column names 
+names(photo_i_adj) <- c("r", "g", "b")
+photo_i_adj_mask <- predict(photo_i_adj, rf_mask_adj)
+plot(photo_i_adj_mask)
+
+photo_j_adj <- rast("Katz lab/texas/tx 2026 drone pics/2026 quadrat pics/cropped_quadrat_pics/wade_t3_tree_adj.tif")
+plotRGB(photo_j_adj)
+names(photo_j_adj) <- c("r", "g", "b")
+photo_j_adj_mask <- predict(photo_j_adj, rf_mask_adj)
+plot(photo_j_adj_mask)
+
+#add mask layer back to original photo
+photo_i_adj <- c(photo_i_adj, photo_i_adj_mask)
+plot(photo_i_adj)
+# plot(photo_i_adj$class) 
+# plot(photo_i_adj$r)
+
 # applying rf pixel classifier to all aerial drone images and saving in new folder ---------------------
 
 photo_dir <- "C:/Users/hmz25/Box/Katz lab/texas/tx 2026 drone pics/2026 quadrat pics/cropped_quadrat_pics"
@@ -58,21 +81,28 @@ if (!dir.exists(output_subfolder)) {
   dir.create(output_subfolder, recursive = TRUE, showWarnings = TRUE)
 }
 
-i = 7
+# i = 7
 
-for(i in 1:length(photo_list)){
+for(i in seq_along(photo_list)){
   photo_i <- raster::stack(photo_list_full_dir[i])  #plotRGB(photo_i)
   
   photo_i_df <- as.data.frame(photo_i) %>%
     rename(c("r" = 1 , "g" = 2, "b" = 3))  #head(photo_i_df)
   
-  photo_i_mask_df <- predict(rf_mask, photo_i_df)
-  # head(photo_i_mask_df)
-  # photo_i_df <- photo_i_df %>%
-  #   mutate(is_foreground = photo_i_mask_df,
-  #          is_foreground_numeric = case_when( is_foreground == "yes" ~ 1,
-  #                                             is_foreground == "not" ~ 0))
-  # #head(photo_i_df)
+  #if basename contains "_adj", apply rf trained on adjusted pixels. if not, apply regular rf
+  if (str_detect(basename(photo_list_full_dir[i]), "_adj")) {
+    photo_i_mask_df <- predict(rf_mask_adj, photo_i_df)
+  } else {
+    photo_i_mask_df <- predict(rf_mask, photo_i_df)
+  }
+  
+  # photo_i_mask_df <- predict(rf_mask, photo_i_df)
+  # # head(photo_i_mask_df)
+  # # photo_i_df <- photo_i_df %>%
+  # #   mutate(is_foreground = photo_i_mask_df,
+  # #          is_foreground_numeric = case_when( is_foreground == "yes" ~ 1,
+  # #                                             is_foreground == "not" ~ 0))
+  # # #head(photo_i_df)
   
   photo_i <- addLayer(photo_i, photo_i[[3]])
   photo_i[[4]] <- photo_i_mask_df # photo_i_df$is_forground
@@ -86,3 +116,7 @@ for(i in 1:length(photo_list)){
 test <- stack("Katz lab/texas/tx 2026 drone pics/2026 quadrat pics/cropped_quadrat_pics/masked_cropped_quadrat_pics/gun_t10_tree.tif")
 plotRGB(test)
 plot(test)
+
+test_adj <- stack("Katz lab/texas/tx 2026 drone pics/2026 quadrat pics/cropped_quadrat_pics/masked_cropped_quadrat_pics/cathedral_t8_tree_adj.tif")
+plotRGB(test_adj)
+plot(test_adj)
